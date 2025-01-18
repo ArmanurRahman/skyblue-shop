a4d7f623b0ebbff5ac09ec3152fbb8d53ae71361
Armanur Rahman <mubeenarman19@gmail.com>
2025-01-18 15:18:42 +0900

    Add product details page

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,14 +1,25 @@
 "use server";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCT_LIMIT } from "../constants";
 
 export async function getLatestProduct() {
-    const prisma = new PrismaClient();
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCT_LIMIT,
         orderBy: { createdAt: "desc" },
     });
+    // return data.map((product) => ({
+    //     ...product,
+    //     price: product.price.toString(),
+    //     rating: product.rating.toString(),
+    // }));
     return convertToPlainObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+    const data = prisma.product.findFirst({
+        where: { slug },
+    });
+    return data;
 }
--- lib/validation.ts
+++ lib/validation.ts
@@ -2,9 +2,10 @@ import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
 const currency = z
-    .number()
+    .string()
     .refine(
-        (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
+        (value) =>
+            /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
         "Price must have exact 2 decimal point"
     );
 export const insertProductSchema = z.object({
@@ -16,6 +17,6 @@ export const insertProductSchema = z.object({
     stock: z.coerce.number(),
     images: z.array(z.string()).min(1, "Product must have at least 1 image"),
     isFeatured: z.boolean(),
-    banner: z.string(),
+    banner: z.string().nullable(),
     price: currency,
 });
