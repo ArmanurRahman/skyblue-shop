18b1a92e5125cdd7849a18605b37ca00c8e5f3c0
Armanur Rahman <mubeenarman19@gmail.com>
2025-01-25 15:06:27 +0900

    Implement search feature

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,26 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProduct } from "@/lib/actions/product.action";
+import ViewAllProductsButton from "@/components/view-all-products-button";
+import {
+    getFeaturedProducts,
+    getLatestProduct,
+} from "@/lib/actions/product.action";
 
 export default async function Home() {
     const latestProducts = await getLatestProduct();
+    const featureProduct = await getFeaturedProducts();
+    console.log(featureProduct);
     return (
         <>
+            {featureProduct.length > 1 && (
+                <ProductCarousel data={featureProduct} />
+            )}
             <ProductList
                 data={latestProducts}
                 title='Recent Products'
                 limit={4}
             />
+            <ViewAllProductsButton />
         </>
     );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,13 +2,16 @@ import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "./menu";
+import CategoryDrawer from "./category-drawer";
+import Search from "./search";
 
 const Header = () => {
     return (
         <header className='w-full border-b'>
             <div className='wrapper flex-between'>
                 <div className='flex-start'>
-                    <Link href='/' className='flex-start'>
+                    <CategoryDrawer />
+                    <Link href='/' className='flex-start ml-4'>
                         <Image
                             src='/images/logo.svg'
                             alt={`${APP_NAME} Logo`}
@@ -20,6 +23,9 @@ const Header = () => {
                     <span className='hidden lg:block font-bold text-2xl ml-3'>
                         {APP_NAME}
                     </span>
+                </div>
+                <div className='hidden md:block'>
+                    <Search />
                 </div>
                 <Menu />
             </div>
